0f7d82906c56e3ef8e646d0feb59cae08f11839d
adarsh bhayani <nobuddy4325@gmail.com>
2023-08-12 17:45:23 +0530

    change value of word at starting time

--- src/component/Textform.js
+++ src/component/Textform.js
@@ -11,25 +11,42 @@ export default function Textform(props) {
         settext(text.toLowerCase());
     }
 
+    const cleartextfunc = () => {
+        settext('');
+    }
+
     const handleonchange = (event) => {
         settext(event.target.value);
+        if(event.target.value == '')
+        {
+            setcount(0);
+        }
+        else
+        {
+        const c = text.split(' ').length;
+        setcount(c);
+        }
+    
     }
-    const [text, settext] = useState("Enter text here");
+    
+    const [text, settext] = useState("");
+    const [count,setcount] = useState(0);
 
     return (
         <div className='bg-dark text-white border border-primary my-3 rounded '>
             <div>
                 <h1 className='my-3'>{props.heading}</h1>
                 <div classNameName="mb-3">
-                    <textarea className="form-control my-3 border border-info " value={text} onChange={handleonchange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                    <textarea className="form-control my-3 border border-info " value={text} onChange={handleonchange}  id="exampleFormControlTextarea1" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary my-3 mx-3 border border-white" onClick={convertUpcase}>Convert to Uppercase</button>
                 <button className="btn btn-primary my-3 mx-3 border border-white" onClick={convertLocase}>Convert to lowercase</button>
+                <button className="btn btn-primary my-3 mx-3 border border-white" onClick={cleartextfunc}>Clear</button>
             </div>
             <div className="container my-3">
                 <h1>Your text summary</h1>
                 <ul className="list-group list-group-flush border border-info rounded">
-                    <li className="list-group-item border border-info">{text.split(" ").length} Words</li>
+                    <li className="list-group-item border border-info">{count} Words</li>
                     <li className="list-group-item border border-info"> {text.length} characters</li>
                     <li className="list-group-item border border-info">it will be take {0.008 * text.split(" ").length} Minutes</li>
                 </ul>
