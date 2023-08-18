21bfae01de317db4e6d031608618c73da4be833b
adarsh bhayani <nobuddy4325@gmail.com>
2023-08-18 19:27:51 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 import Navbar from './component/Navbar';
 import Textform from './component/Textform';
-
+import Alert from './component/Alert';
+import About from './component/About';
 function App() {
+  const [alert,setalert] = useState(null);
+
+    const showalert = (message,type) =>{
+          setalert({
+            msg: message,
+            type: type
+          })
+          
+          setTimeout(() => {
+            setalert(null)
+          }, 2000);
+    }
   return (
     <>
        <Navbar title="adarsh" />
+       <Alert alert={alert}/>
        <div className="container">
-       <Textform heading="Enter the text below to analyze"/>
+        {/* <About/> */}
+       <Textform showalert={showalert} heading="Enter the text below to analyze" />
        </div>
  
     </>
--- src/component/Textform.js
+++ src/component/Textform.js
@@ -1,55 +1,80 @@
 import React, { useState } from 'react'
 
+
 export default function Textform(props) {
     const convertUpcase = () => {
         // const newtext = text.toUpperCase();
         settext(text.toUpperCase());
+
+        var txt = document.getElementById("myid");
+        if (!(txt.value === "")) {
+            props.showalert(" convert to uppercase successfully", "success");
+        }
+
     }
 
     const convertLocase = () => {
         // const newtext = text.toUpperCase();
         settext(text.toLowerCase());
+
+        var txt = document.getElementById("myid");
+        if (!(txt.value === "")) {
+            props.showalert(" convert to lowercase successfully", "success");
+        }
+
     }
 
     const cleartextfunc = () => {
+        var text = document.getElementById("myid");
         settext('');
         setcount(0);
+        if (!(text.value === "")) {
+            props.showalert(" clear successfully", "success");
+        }
+
     }
 
     const handleonchange = (event) => {
         settext(event.target.value);
-        if(event.target.value == '')
-        {
+        if (event.target.value === '') {
             setcount(0);
         }
-        else
-        {
-        const c = text.split(' ').length;
-        setcount(c);
+        else {
+            const c = text.split(' ').length;
+            setcount(c);
         }
-    
+
     }
-    
+    const copytext = () => {
+        var text = document.getElementById("myid");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        if (!(text.value === "")) {
+            props.showalert("copied to clipboard", "success");
+        }
+
+    }
     const [text, settext] = useState("");
-    const [count,setcount] = useState(0);
+    const [count, setcount] = useState(0);
 
     return (
         <div className='bg-dark text-white border border-primary my-3 rounded border border-3'>
             <div>
                 <h1 className='my-3'>{props.heading}</h1>
                 <div classNameName="mb-3">
-                    <textarea className="form-control my-3 border border-info " value={text} onChange={handleonchange}  id="exampleFormControlTextarea1" rows="8"></textarea>
+                    <textarea className="form-control my-3 border border-info " value={text} onChange={handleonchange} id="myid" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary my-3 mx-3 border border-white" onClick={convertUpcase}>Convert to Uppercase</button>
                 <button className="btn btn-primary my-3 mx-3 border border-white" onClick={convertLocase}>Convert to lowercase</button>
                 <button className="btn btn-primary my-3 mx-3 border border-white" onClick={cleartextfunc}>Clear</button>
+                <button className="btn btn-primary my-3 mx-3 border border-white" onClick={copytext}>Copy text</button>
             </div>
             <div className="container my-3">
                 <h1>Your text summary</h1>
                 <ul className="list-group list-group-flush border border-info rounded">
                     <li className="list-group-item border border-info">{count} Words</li>
                     <li className="list-group-item border border-info"> {text.length} characters</li>
-                    <li className="list-group-item border border-info">it will be take {0.008 * count} Minutes</li>
+                    <li className="list-group-item border border-info">it will be take {0.008 * count} Minutes to read</li>
                 </ul>
                 <h2 className='my-3'>Preview</h2>
                 <p>{text}</p>
